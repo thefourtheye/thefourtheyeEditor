ecd99cc6acc6690ab88358e9d02f4bdd0c5a9eed
thefourtheye <thechargingvolcano@gmail.com>
2013-09-08 07:29:56 +0530

    Fix for extra flower bracket in python tuple and fixed formatting

--- src/thefourtheyeEditor/supportedLanguages/Python.java
+++ src/thefourtheyeEditor/supportedLanguages/Python.java
@@ -9,51 +9,55 @@ import com.topcoder.shared.problem.TestCase;
 
 public class Python extends Common
 {
-    public Python(Language lang, ProblemComponentModel component) throws Exception
-    {
-       super(lang, component);
-    }
-    
-    private String getData(int dim, String data)
-    {
-        return dim == 0?data:"(" + data.substring(1, data.length() - 1) + ",)"; 
-    }
-    
-    @Override
-    public ArrayList<String> getTestSuite()
-    {
-       DataType[] paramTypes = getParamTypes();
-       TestCase[] testCases = getTestCases();
-       String invokeLine = "";
-       for (int i = 0; i < testCases.length; i++)
-       {
-          String[] inputParams = testCases[i].getInput();
-          if (invokeLine == "")
-          {
-             invokeLine = getClassName() + "()." + getMethodName()+ "(";
-             for (int j = 0; j < inputParams.length; j++)
-             {
-                invokeLine += "args" + String.valueOf(j)
-                      + (j != inputParams.length - 1 ? "," : ")");
-             }
-          }
-          for (int j = 0; j < inputParams.length; j++)
-          {
-             String inputParamDefn = "args" + String.valueOf(j) + " = ";
-             inputParamDefn += getData(paramTypes[j].getDimension(), inputParams[j]);
-             testSuite.add(inputParamDefn);
-          }
-          String outputParamDefn = "expected = " + 
-                getData(getReturnType().getDimension(), testCases[i].getOutput());
-          testSuite.add(outputParamDefn);
-          testSuite.add("eq(" + i + ", expected, " + invokeLine +")");
-          testSuite.add("");
-       }
-       return testSuite;
-    }
+   public Python(Language lang, ProblemComponentModel component)
+         throws Exception
+   {
+      super(lang, component);
+   }
 
-    public String getFileExtension()
-    {
-       return ".py";
-    }
+   private String getData(int dim, String data)
+   {
+      return dim == 0 ? data : "("
+            + data.trim().substring(1, data.trim().length() - 1) + ",)";
+   }
+
+   @Override
+   public ArrayList<String> getTestSuite()
+   {
+      DataType[] paramTypes = getParamTypes();
+      TestCase[] testCases = getTestCases();
+      String invokeLine = "";
+      for (int i = 0; i < testCases.length; i++)
+      {
+         String[] inputParams = testCases[i].getInput();
+         if (invokeLine == "")
+         {
+            invokeLine = getClassName() + "()." + getMethodName() + "(";
+            for (int j = 0; j < inputParams.length; j++)
+            {
+               invokeLine += "args" + String.valueOf(j)
+                     + (j != inputParams.length - 1 ? "," : ")");
+            }
+         }
+         for (int j = 0; j < inputParams.length; j++)
+         {
+            String inputParamDefn = "args" + String.valueOf(j) + " = ";
+            inputParamDefn += getData(paramTypes[j].getDimension(),
+                  inputParams[j]);
+            testSuite.add(inputParamDefn);
+         }
+         String outputParamDefn = "expected = "
+               + getData(getReturnType().getDimension(),
+                     testCases[i].getOutput());
+         testSuite.add(outputParamDefn);
+         testSuite.add("eq(" + i + ", expected, " + invokeLine + ")");
+         testSuite.add("");
+      }
+      return testSuite;
+   }
+
+   public String getFileExtension()
+   {
+      return ".py";
+   }
 }
